c22df89c357e627cf0d9f549f988fe454711a823
Mountain Scott <mountainscott@yahoo.com>
2021-08-01 11:57:36 -0700

    use safe_str_com

--- security.py
+++ security.py
@@ -1,17 +1,23 @@
 #in-memory table of registered users
-
+from werkzeug.security import safe_str_cmp #, generate_password_hash, check_password_hash
+from user import User
 users = [
-    {"id" : 1, "name" : "John", "username" : "john", "password" : "john123"},
-    {"id" : 2, "name" : "Michael", "username" : "michael", "password" : "michael123"},
+  User(1, "bob", "asdf"),
+  User(2, "susan", "xyz"),
+  User(3, "mary", "mno"),
+  User(4, "david", "qwer"),
+
 ]
 
-username_mapping = {u["username"] : u for u in users}  #access dictionary by username
+username_mapping = {u.username: u for u in users}  #access dictionary by username
 
-userid_mapping = {u["id"] : u for u in users}  #access dictionary by userid
+userid_mapping = {u.id: u for u in users} #access dictionary by userid
 
 def authenticate(username, password): 
     user = username_mapping.get(username, None)
-    if user and user["password"] == password:
+    #you can run into problems when comparing strings with each other
+    #(e.g. ascii vs unicode, so use safe_str_cmp)
+    if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload): #payload is the contents of the JWT token
